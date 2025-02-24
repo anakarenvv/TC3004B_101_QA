36c717916a10a7c3f3efd21ef4d79d849f951be5
Ana Karen <122408335+anakarenvv@users.noreply.github.com>
2025-02-24 08:02:06 -0600

    Update test6.spec.js

--- tests/test6.spec.js
+++ tests/test6.spec.js
@@ -37,7 +37,8 @@ describe('test1', function() {
 
             // Close the browser
             await driver.quit();
-        }
+          });
+
 
   it('test6', async function() {
     await driver.get("http://127.0.0.1:5500/")
