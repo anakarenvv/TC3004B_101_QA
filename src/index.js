f5b58b8b7422c7c5383706c39febf4245c20e3c1
Ana Karen <A01067716@tec.mx>
2025-02-24 12:28:03 -0600

    substract button added + test cases

--- src/index.js
+++ src/index.js
@@ -9,10 +9,18 @@ function calculate(){
     
 }
 
-function resetFields(){
-document.getElementsById("num1").value="";
-document.getElementsById("num2").value="";
-document.getElementsById("operator").value="+";
-document.getElementsById("result").value="Result:";
 
+function subtract() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    let result = Number(num1) - Number(num2);
+    document.getElementById("result").innerText = "Result: " + result.toFixed(2);
+}
+
+function resetFields() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("operator").innerText = "+";
+    document.getElementById("result").innerText = "Result:";
 }
