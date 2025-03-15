da54c0ab73c2e5e135a3a5b259a22be41c9973c8
Ana Karen <A01067716@tec.mx>
2025-03-15 15:46:39 -0600

    Back and front

--- backend/src/main/java/com/example/demo/CalculatorController.java
+++ backend/src/main/java/com/example/demo/CalculatorController.java
@@ -2,25 +2,24 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*") // Permitir llamadas desde cualquier origen
 public class CalculatorController {
 
     @GetMapping("/add")
     public Calculator add(
             @RequestParam(value = "num1") double num1,
             @RequestParam(value = "num2") double num2) {
-        double result = num1 + num2;
-        return new Calculator(result);
+        return new Calculator(num1 + num2);
     }
 
     @GetMapping("/sub")
     public Calculator sub(
             @RequestParam(value = "num1") double num1,
             @RequestParam(value = "num2") double num2) {
-        double result = num1 - num2;
-        return new Calculator(result);
+        return new Calculator(num1 - num2);
     }
 }
-
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,25 @@
-function calculate(){
+async function calculate() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
 
-    const num1= document.getElementById("num1").value;
-    const num2= document.getElementById("num2").value;
+    const response = await fetch(`http://localhost:8080/add?num1=${num1}&num2=${num2}`);
+    const data = await response.json();
 
-        let result;
-        result= Number(num1) + Number (num2);
-        document.getElementById("result").innerText="Result:" + result.toFixed(2);
-    
+    document.getElementById("result").innerText = `Result: ${data.result}`;
 }
 
+async function subtract() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
 
-function subtract() {
-    const num1 = document.getElementById("num1").value;
-    const num2 = document.getElementById("num2").value;
+    const response = await fetch(`http://localhost:8080/sub?num1=${num1}&num2=${num2}`);
+    const data = await response.json();
 
-    let result = Number(num1) - Number(num2);
-    document.getElementById("result").innerText = "Result: " + result.toFixed(2);
+    document.getElementById("result").innerText = `Result: ${data.result}`;
 }
 
 function resetFields() {
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
-    document.getElementById("operator").innerText = "+";
     document.getElementById("result").innerText = "Result:";
 }
